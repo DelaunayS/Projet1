5204db5756faf24bce981be9d64b5af8f228b0fe
DelaunayS <sebolix@hotmail.fr>
2022-10-12 16:01:25 +0200

    Ajout de bin/ et fichier stagiaire texte

--- src/fr/isika/cda21/projet1/annuaire/Lanceur.java
+++ src/fr/isika/cda21/projet1/annuaire/Lanceur.java
@@ -1,8 +1,9 @@
 package fr.isika.cda21.projet1.annuaire;
 	
+import fr.isika.cda21.projet1.annuaire.utilitaires.Fichier;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
@@ -10,7 +11,10 @@ public class Lanceur extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Pane root = new Pane();
+			Fichier fichierTxt=new Fichier("Fichier texte");
+			fichierTxt.lectureFichier();
+			System.out.println(fichierTxt.toString());
+			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,800,600);
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/fr/isika/cda21/projet1/annuaire/utilitaires/Fichier.java
+++ src/fr/isika/cda21/projet1/annuaire/utilitaires/Fichier.java
@@ -1,5 +1,72 @@
 package fr.isika.cda21.projet1.annuaire.utilitaires;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Fichier {
+
+	private static String cheminFichier = "./src/fr/isika/cda21/projet1/annuaire/utilitaires/STAGIAIRES.DON";
+
+	// attributs
+	private String nom;
+	private String chemin;
+	private ArrayList<String> lignesDuFichier = new ArrayList<String>();
+
+	// getters
+	public String getNom() {
+		return nom;
+	}
+
+	public String getChemin() {
+		return chemin;
+	}
+
+	public ArrayList<String> getLignesDuFichier() {
+		return lignesDuFichier;
+	}
+
+	// constructeurs
+	public Fichier(String nom) {
+		this.nom = nom;
+		this.chemin = cheminFichier;
+	}
+
+	public Fichier() {
+
+	}
+
+	// méthodes spécifiques
+	@Override
+	public String toString() {
+
+		String message = "Fichier" + nom + " dans " + chemin + "\n";
+		String listeDeLignes = "";
+		for (String ligne : lignesDuFichier) {
+			listeDeLignes += ligne + "\n";
+		}
+		message += listeDeLignes;
+
+		return message;
+	}
+	
+	// lit le fichier et ajoute chaque ligne dans un ArrayList lignesDuFichier
+		public void lectureFichier() {
+
+			try (BufferedReader bufferredReader = new BufferedReader(new FileReader(getChemin()));) {
+
+				String line;
+				while ((line = bufferredReader.readLine()) != null) {
+					getLignesDuFichier().add(line);
+				}
+
+			} catch (FileNotFoundException e) {
+				System.err.println(e);
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+		}
 
 }
